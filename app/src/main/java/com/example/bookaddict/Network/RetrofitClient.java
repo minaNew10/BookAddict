ba81b59f31441cfca136b11eb6c10a8a8fd3e89c
MinaN <minadevdream@gmail.com>
2020-02-06 23:57:05 +0200

    add auto logging

--- app/src/main/java/com/example/bookaddict/Network/RetrofitClient.java
+++ app/src/main/java/com/example/bookaddict/Network/RetrofitClient.java
@@ -5,6 +5,8 @@ import android.provider.SyncStateContract;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -18,6 +20,7 @@ public class RetrofitClient {
     private static Retrofit getInstance() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
+                    .client(buildClient())
                     .baseUrl("https://www.googleapis.com/books/v1/")
                     .addConverterFactory(GsonConverterFactory.create())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
@@ -32,6 +35,15 @@ public class RetrofitClient {
             ApiService = getInstance().create(ApiService.class);
         }
         return ApiService;
+    }
+
+    private static OkHttpClient buildClient() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.level(HttpLoggingInterceptor.Level.BODY);
+        return new OkHttpClient
+                .Builder()
+                .addInterceptor(logging)
+                .build();
     }
 
 }
